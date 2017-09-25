56efee0c6484affa9dae1a5116a5a9c28ca3cb36
Remilia <HinanawiCirno@outlook.com>
2017-09-25 14:13:34 +0800

    init project

---
+++ src/main/java/me/mushen/examples/jvm/Test.java
@@ -0,0 +1,13 @@
+package me.mushen.examples.jvm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2017-09-25
+ */
+public class Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+}
---
+++ src/main/java/me/mushen/examples/jvm/chapter2/SimpleArgs.java
@@ -0,0 +1,10 @@
+package me.mushen.examples.jvm.chapter2;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2017-09-25
+ */
+public class SimpleArgs {
+
+}
